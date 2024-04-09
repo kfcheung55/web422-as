919e9c2daaa9c45d671f1619dec8eb3ceade3342
Sandy Cheung <kcheung55@myseneca.ca>
2024-04-09 00:41:51 -0400

    as06

---
+++ user-api/server.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const userService = require("./user-service.js");
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+// const app = express();
+
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+require('dotenv').config();
+
+// Configure its options
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+
+  if (jwt_payload) {
+    // The following will ensure that all routes using
+    // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values
+    // that matches the request payload data
+    next(null, {
+      _id: jwt_payload._id,
+      userName: jwt_payload.userName,
+    //   fullName: jwt_payload.fullName,
+    //   role: jwt_payload.role,
+    });
+  } else {
+    next(null, false);
+  }
+});
+
+app.use(express.json());
+app.use(cors());
+const authenticateJWT = passport.authenticate('jwt', { session: false });
+
+
+app.post("/api/user/register",authenticateJWT, (req, res) => {
+    userService.registerUser(req.body)
+    .then((msg) => {
+        res.json({ "message": msg });
+    }).catch((msg) => {
+        res.status(422).json({ "message": msg });
+    });
+});
+
+app.post("/api/user/login", authenticateJWT,(req, res) => {
+    userService.checkUser(req.body)
+    .then((user) => {
+        if(user){
+            let payload = {
+                _id: user._id,
+                userName: user.userName,
+              };
+            let token = jwt.sign(payload, process.env.JWT_SECRET);
+            res.json({ 
+                "message": "login successful",
+                token: token
+            });
+
+            }
+    }).catch(msg => {
+        res.status(422).json({ "message": msg });
+    });
+});
+
+app.get("/api/user/favourites", authenticateJWT,(req, res) => {
+    userService.getFavourites(req.user._id)
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+
+});
+
+app.put("/api/user/favourites/:id",authenticateJWT, (req, res) => {
+    userService.addFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.delete("/api/user/favourites/:id",authenticateJWT, (req, res) => {
+    userService.removeFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.get("/api/user/history",authenticateJWT, (req, res) => {
+    userService.getHistory(req.user._id)
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+
+});
+
+app.put("/api/user/history/:id", authenticateJWT,(req, res) => {
+    userService.addHistory(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.delete("/api/user/history/:id",authenticateJWT, (req, res) => {
+    userService.removeHistory(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+userService.connect()
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+})
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
